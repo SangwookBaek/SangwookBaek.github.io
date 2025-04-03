5a81cba57d4c7019a4c6fc10e0f6b73b166bc781
SangookBaek <mindustrylab@gmail.com>
2025-04-03 22:08:19 +0900

    fix

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743669640',resources: [
+    cacheName: 'chirpy-1743685671',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
