c85e4991e611d1721dad6f0903636d7890015f5b
SangookBaek <mindustrylab@gmail.com>
2025-04-16 15:06:18 +0900

    add sitemap

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744475182',resources: [
+    cacheName: 'chirpy-1744783559',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
